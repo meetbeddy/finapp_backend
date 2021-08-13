7aedda40df9d78152ee044c08642aeea241c5507
obed okpala <katlybedrick@gmail.com>
2021-08-13 18:28:53 +0100

    changes to engine version

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.use(cors());
 app.use(express.json());
 
 //database connect
-const db = "mongodb://localhost:27017/finapp";
+// const db = "mongodb://localhost:27017/finapp";
+const db =
+  "mongodb+srv://meetbeddy:meetbeddy@cluster0.onuw2.mongodb.net/userdb?retryWrites=true&w=majority";
+
 mongoose
   .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
-//handle routes
+//handle route
 app.use("/user", user);
 
 //start app
