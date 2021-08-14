adc9439001d387af543ce81318e98b3fe88e0c4c
obed okpala <katlybedrick@gmail.com>
2021-08-14 16:51:54 +0100

    correction

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../../controllers/adminControllers");
 
-router.post("/API/confirm/:userid", adminController.confirmUser);
+router.post("/API/confirm/:id", adminController.confirmUser);
 router.get("/API/getmembers", adminController.FetchUsers);
 
 module.exports = router;
